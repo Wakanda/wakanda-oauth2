8c29dd6ed572bf5cd1239570607d2494417df732
Yann Jégo <yann.jego@wakanda.io>
2015-10-20 15:39:14 +0200

    Fix CSRF missing in sessionStorage after createUserSession()

--- oauth2/handlers.js
+++ oauth2/handlers.js
@@ -15,15 +15,15 @@
 	var urlQuery	= request.rawURL.split( '?' )[ 1 ];//workaround
 	
 	var params		= tools.parseQueryString( urlQuery );
-
+	
     var redirectTo	= require( 'oauth2-provider-' + params.provider ).getRedirectURL( { 'CSRF' : CSRF , 'provider' : params.provider, 'scope': params.scope } );
-
+	
     sessionStorage[ config._SESSION.CSRF ] = CSRF;
-    
+	
     response.headers['location']	= redirectTo;
     
     response.statusCode				= 302;
-
+	
     return '';
 };
 
@@ -38,7 +38,7 @@ function callback( request , response ) {
 	var params		= tools.parseQueryString( urlQuery );
 	var state		= ( typeof params.state[ 0 ] == 'string' ) ? tools.parseQueryString( params.state[ 0 ] ) : undefined;
 	var provider	= state.from[ 0 ];
-
+	
 	/**
 	 * Check provider authorisation error
 	 * Redirect on wakanda failure page. Return through url params a wakanda 'error'
@@ -57,7 +57,7 @@ function callback( request , response ) {
 	 * Verify that the CSRF parameter value corresponds to the user's session.
 	 * Redirect on wakanda failure page. Return through url params a wakanda 'error'
 	 */
-
+	
     if ( ! sessionStorage[ config._SESSION.CSRF ] && sessionStorage[ config._SESSION.CSRF ] != state[ 'CSRF' ][ 0 ] )
 		return error.redirectUrl(response, 'invalid_CSRF');
     
@@ -83,21 +83,30 @@ function callback( request , response ) {
 	 */
     if ( !exchangeResponse.email )
 		return error.redirectUrl(response, 'missing_user_email');
+
+	/*
+	 * Keep CSRF in session storage. Session storage is reset with createUserSession()
+	 */
+	
+	var CSRF = sessionStorage[ config._SESSION.CSRF ];
 	
 	/**
 	 * oauth2 authentification success. Create/Update a Wakanda user session.
 	 */
 	createWakSession( exchangeResponse );
-    createOAuth2Session( exchangeResponse ); // SessionStorage must be set after the user session creation. Otherwise, the sessionStorage will be reset.
+	
+	/*
+	 * Save again the CSRF in the session storage
+	 * SessionStorage must be set after the user session creation. Otherwise, the sessionStorage will be reset.
+	 */
+	sessionStorage[ config._SESSION.CSRF ] = CSRF;
+	sessionStorage[ config._SESSION.EMAIL ] = exchangeResponse.email;
+	sessionStorage[ config._SESSION.TOKEN ] = exchangeResponse.token;
+	
 	response.headers['location'] = config.redirectOnSuccess;
 	response.statusCode = 302;
 	
 	return response;
-}
-
-function createOAuth2Session( info ) {
-	sessionStorage[ config._SESSION.EMAIL ] = info.email;
-	sessionStorage[ config._SESSION.TOKEN ] = info.token;
 }
 
 function createWakSession( info ) {
@@ -116,7 +125,7 @@ function createWakSession( info ) {
 		user.UID = generateUUID();
 		user.email = info.email;
 		user.save( );
-	}	
+	}
 	
 	/*
 	 * Create a wakanda user session
